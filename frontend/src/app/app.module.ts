2f4b33d19d9fb4898d449cedea1f8d9e713a0cf4
oleksandr.huba <oleksandr.huba@stud.uni-bamberg.de>
2024-04-21 13:38:49 +0200

    create frontend app

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingComponent } from './components/landing/landing.component';
+
+@NgModule({
+  declarations: [AppComponent, NavigationComponent, FooterComponent, LandingComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
